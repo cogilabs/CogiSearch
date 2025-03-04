bd1688fc54ced798623610c7e785203e90c775bd
David Girou <davgi.wifeo@gmail.com>
2025-03-04 20:34:53 +0100

    Update customBangs.js

---
+++ customBangs.js
@@ -0,0 +1,20 @@
+const customBangs = [
+  {
+    "c": "Entertainment",
+    "d": "goclecd.fr",
+    "r": 0,
+    "s": "GoCleCD",
+    "sc": "Games+(general)",
+    "t": "gcd",
+    "u": "https://www.goclecd.fr/produits/?search_name={{{s}}}"
+  },
+  {
+    "c": "Tech",
+    "d": "support.watteco.com",
+    "r": 0,
+    "s": "Watteco Support",
+    "sc": "Tools",
+    "t": "wtc",
+    "u": "https://support.watteco.com/?s={{{s}}}"
+  }
+];
