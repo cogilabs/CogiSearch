8112dfdd1553c2a6d105143fa3d7020377ba8fb6
David Girou <david.girou@0Davgi0.com>
2025-04-30 17:29:08 +0200

    Implement autocomplete feature for bang suggestions in search input Add "Search Cogilabs" to custom bangs

--- customBangs.js
+++ customBangs.js
@@ -2,7 +2,7 @@ const customBangs = [
   {
     "c": "Entertainment",
     "d": "goclecd.fr",
-    "r": 0,
+    "r": 80,
     "s": "GoCleCD",
     "sc": "Games+(general)",
     "t": "gcd",
@@ -11,7 +11,7 @@ const customBangs = [
   {
     "c": "Tech",
     "d": "support.watteco.com",
-    "r": 0,
+    "r": 10,
     "s": "Watteco Support",
     "sc": "Tools",
     "t": "wtc",
@@ -20,7 +20,7 @@ const customBangs = [
   {
     "c":"Online Services",
     "d":"www.google.com",
-    "r": 0,
+    "r": 2000000,
     "s":"Google Web Tab",
     "sc":"Google",
     "t": "gweb",
@@ -29,10 +29,19 @@ const customBangs = [
   {
     "c": "Tech",
     "d": "lora.watteco.fr",
-    "r": 0,
+    "r": 10,
     "s": "Watteco Decoder",
     "sc": "Tools",
     "t": "wtcd",
     "u": "https://lora.watteco.fr/Lora/Decoders/index.php?trame={{{s}}}&MySelectMenu=0&+submit=Submit"
+  },
+  {
+    "c": "Tech",
+    "d": "cogilabs.eu",
+    "r": 500,
+    "s": "Cogilabs.eu",
+    "sc": "Blogs",
+    "t": "cogilabs",
+    "u": "https://www.google.com/search?udm=14&q=site%3Acogilabs.eu%20{{{s}}}"
   }
 ];
